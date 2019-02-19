addfd90a0c1c859b2532c93fa403047b7f363dc9
Patrick Kim <pkim050@gmail.com>
2019-02-19 08:31:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return true if self.end_with?(".")
+    return false
   end
 
   def question?
-
+    return true if self.end_with?("?")
+    return false
   end
 
   def exclamation?
-
+    return true if self.end_with?("!")
+    return false
   end
 
   def count_sentences
-
+    
   end
 end
